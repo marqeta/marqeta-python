84567b668fcff99563ba5963d4519428c67735d7
Jonathan  Vasquez <jonalexandervasquez@gmail.com>
2022-06-14 11:24:14 -0400

    version bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name="marqeta",
     # Manually adding version number due to issues with poetry when calling __version__
     # This has to be updated alongside version.py when the version changes
-    version="0.2.3",
+    version="0.2.4",
     description="Marqeta Python SDK",
     author="Marqeta, Inc.",
     url="https://github.com/marqeta/marqeta-python",
